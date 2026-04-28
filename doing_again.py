670d631a4ae127be4a7bfc0a4b33df9544a9d681
Shivam Pandey <shivamrajeshpandey9136@gmail.com>
2026-04-28 19:30:00 +0530

    Add variables and data types practice

--- doing_again.py
+++ doing_again.py
@@ -245,13 +245,21 @@ and a list of your 3 hobbies. Print each value using its key."""
 # fact=1  
 # for i in range(1,n+1,1):
 #     fact *= i
-# print(fact)      
+# # print(fact)      
 
-n = int(input("Enter the number: "))
+# n = int(input("Enter the number: "))
 
-fact = 1
+# fact = 1
 
-for i in range(1, n + 1):
-    fact *= i
+# for i in range(1, n + 1):
+#     fact *= i
 
-print(fact)
+# print(fact)
+
+# A,B="2","3"
+# print(A+B)
+
+name = "Shivam"
+age = 21
+
+print(name, age)
